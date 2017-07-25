83f627c043a2cf33de889e76e80696ac7537e9cc
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-25 10:47:19 -1000

    denormalize

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -14,7 +14,18 @@ DROP TABLE IF EXISTS car_models;
 
 SELECT DISTINCT make_title
 FROM car_models;
+-- STEP 1
 
 SELECT DISTINCT model_title
 FROM car_models
 WHERE make_code = 'VOLKS';
+-- STEP 2
+
+SELECT DISTINCT make_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+-- STEP 3
+
+SELECT DISTINCT make_code, make_title, year, model_code, model_title
+FROM car_models
+WHERE  year BETWEEN '2010' AND 2015;
