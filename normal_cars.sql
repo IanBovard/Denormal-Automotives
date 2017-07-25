44597b68f8f5629bf5f215ff80eac062d614cb64
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-25 11:47:16 -1000

    3NF changes

--- normal_cars.sql
+++ normal_cars.sql
@@ -19,6 +19,11 @@ CREATE TABLE make (
   make_title character varying(125) NOT NULL
 );
 
+INSERT INTO make VALUES (
+ denormal_cars.make_code,
+ denormal_cars.make_title
+);
+
 CREATE TABLE model (
   id SERIAL PRIMARY KEY NOT NULL,
   model_code character varying(125) NOT NULL,
@@ -26,12 +31,21 @@ CREATE TABLE model (
   make_id INT REFERENCES make(id)
 );
 
-CREATE TABLE yearDictionary (
+INSERT INTO model (
+  denormal_cars.model_code,
+  denormal_cars.model_title
+);
+
+CREATE TABLE year_dictionary (
   id SERIAL PRIMARY KEY NOT NULL,
   year integer NOT NULL
 );
 
+INSERT INTO year_dictionary (
+  (2000), (2001), (2002), (2003), (2004), (2005), (2006), (2007), (2008), (2009), (2010), (2011), (2012), (2013), (2014), (2015), (2016)
+);
+
 CREATE TABLE model_year (
-  model_id INT REFERENCES model (id),
+  model_id INT REFERENCES model(id),
   year_id INT REFERENCES year(id)
 );
